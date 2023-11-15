da3770b1de8d626e3a70a69e459785669628ce84
Swasti008 <swastimohanty452@gmail.com>
2023-11-15 18:10:54 +0530

    done

--- game.js
+++ game.js
@@ -1,5 +1,76 @@
 // Iteration 2: Generate 2 random number and display it on the screen
+let RandomNumber1=0,RandomNumber2=0;
+let div1=document.getElementById("number1");
+let div2=document.getElementById("number2");
 
+function gettingRandom(){
+ RandomNumber1=Math.ceil(Math.random()*100)+1;
+ RandomNumber2=Math.ceil(Math.random()*100)+1;
+div1.innerText=RandomNumber1;
+div2.innerText=RandomNumber2;
+}
+gettingRandom();
 // Iteration 3: Make the options button functional
+var score=0;
+let greaterThan=document.getElementById("greater-than");
+let equalTo=document.getElementById("equal-to");
+let lessThan=document.getElementById("lesser-than");
+greaterThan.addEventListener("click",function(){
+    if (RandomNumber1>RandomNumber2){
+        score++;
+        gettingRandom();
+        resetTime(timing);
+    }
+    else{
+        window.location.href="./gameover.html";
+        localStorage.setItem('score',score);
+    }
+})
 
+equalTo.addEventListener("click",function(){
+    if (RandomNumber1==RandomNumber2){
+        score++;
+        gettingRandom();
+        resetTime(timing);
+    }
+    else{
+        window.location.href="./gameover.html";
+        localStorage.setItem('score',score);
+    }
+})
+
+lessThan.addEventListener("click",function(){
+    if (RandomNumber1<RandomNumber2){
+        score++;
+        gettingRandom();
+        resetTime(timing);
+    }
+    else{
+        window.location.href="./gameover.html";
+        localStorage.setItem('score',score);
+    }
+})
 // Iteration 4: Build a timer for the game
+let time=5;
+let timer=document.getElementById("timer");
+var timing;
+function timerON(){
+    time = 5;
+        timing =setInterval(function(){
+        time--;
+        if (time!=0){
+            timer.innerHTML="<p>"+time+"seconds</p>";
+        }
+        else{
+            window.location.href="./gameover.html"
+        }
+        
+    },1000);
+}
+
+function resetTime(timing) {
+    clearInterval(timing);
+    timerON();
+  }
+  
+  timerON();
--- score.js
+++ score.js
@@ -1,1 +1,9 @@
-// Iteration 5: Store the player score and display it on the game over screen
+// Iteration 5: Store the player score and display it on the game over screenstore
+let storedScore = localStorage.getItem("score");
+let showScore = document.getElementById("score-board");
+showScore.innerHTML=storedScore;
+// For play again button
+let overButton= document.getElementById("play-again-button");
+overButton.addEventListener("click",function(){
+    window.location.href="index.html";
+});
